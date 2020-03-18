0110ce13f54ba99e137cebcec70ed6e26b448454
rebak16 <rebakbalazs@gmail.com>
2020-03-19 00:32:39 +0100

    add all

--- src/main/java/WebDriverManager.java
+++ src/main/java/WebDriverManager.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -9,7 +10,7 @@ import java.net.URL;
 
 public class WebDriverManager {
 
-    private static WebDriver driver = null;
+    private static WebDriver driver = new FirefoxDriver();
     private static String gridUrl = "http://localhost:4444/wd/hub";
 
 
@@ -33,7 +34,7 @@ public class WebDriverManager {
                 e.fillInStackTrace();
             }
         }
-       // System.setProperty("webdriver.chrome.driver", "/src/main/resources/chromedriver");
+        // System.setProperty("webdriver.chrome.driver", "/src/main/resources/chromedriver");
         driver.manage().window().maximize();
         return driver;
     }
