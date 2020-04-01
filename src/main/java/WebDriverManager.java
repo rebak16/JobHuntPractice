7fa9ea221b913ec93ac520d2cab501741185b39c
rebak16 <rebakbalazs@gmail.com>
2020-04-01 19:14:39 +0200

    add all

--- src/main/java/WebDriverManager.java
+++ src/main/java/WebDriverManager.java
@@ -13,7 +13,7 @@ public class WebDriverManager {
     private static String password = System.getenv("PASSWORD");
     private static String hubUser = System.getenv("hubUSERNAME");
     private static String hub = System.getenv("hubURL");
-    private static String hubUrl = "https://" + hubUser + ":" + password + "@" + hub;
+    private static String hubUrl = "https://selenium:CoolCanvas19.@seleniumhub.codecool.codecanvas.hu/wd/hub";
     private static WebDriver driver = null;
 
 
